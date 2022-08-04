2da4acb8c56a403b2ffe2528ed4db919ff03a23d
Nic Klaassen <nic@goteleport.com>
2022-08-04 13:30:37 -0700

    add v1

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/nklaassen/sts-sandbox
 go 1.18
 
 require (
+	github.com/aws/aws-sdk-go v1.44.70
 	github.com/aws/aws-sdk-go-v2/config v1.15.15
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.10
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gravitational/trace v1.1.19
 )
 
 require (
@@ -17,4 +20,11 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.9 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.11.13 // indirect
 	github.com/aws/smithy-go v1.12.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jonboulle/clockwork v0.2.2 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/crypto v0.0.0-20220126234351-aa10faf2a1f8 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/sts"
+	stsv2 "github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	stsv1 "github.com/aws/aws-sdk-go/service/sts"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func check(err error) {
@@ -14,13 +21,13 @@ func check(err error) {
 	}
 }
 
-func main() {
-	ctx := context.Background()
-
+func v2(ctx context.Context) {
 	cfg, err := config.LoadDefaultConfig(ctx)
 	check(err)
 
-	client := sts.NewFromConfig(cfg)
+	spew.Dump(cfg)
+
+	client := stsv2.NewFromConfig(cfg)
 
 	resp, err := client.GetCallerIdentity(ctx, nil)
 	check(err)
@@ -28,4 +35,31 @@ func main() {
 	fmt.Println(*resp.Account)
 	fmt.Println(*resp.Arn)
 	fmt.Println(*resp.UserId)
+}
+
+func v1(ctx context.Context) {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	})
+	check(err)
+
+	stsService := stsv1.New(sess, aws.NewConfig().WithSTSRegionalEndpoint(endpoints.RegionalSTSEndpoint))
+	req, _ := stsService.GetCallerIdentityRequest(&sts.GetCallerIdentityInput{})
+
+	// sign the request, including headers
+	if err := req.Sign(); err != nil {
+		check(err)
+	}
+	// write the signed HTTP request to a buffer
+	var signedRequest bytes.Buffer
+	if err := req.HTTPRequest.Write(&signedRequest); err != nil {
+		check(err)
+	}
+	println(string(signedRequest.Bytes()))
+}
+
+func main() {
+	ctx := context.Background()
+
+	v1(ctx)
 }
